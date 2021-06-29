823fe60e53fb6b7f84b14d8173f8428126b61198
Julian Thong <julianzthong@gmail.com>
2021-06-29 15:14:05 -0700

    Adjusted stress_test file.

--- PostgreSQL/server/server.js
+++ PostgreSQL/server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const db = require('./db');
 const router = require('./routes')
+// const loaderio = require('../loaderio-20c5b1e9a8571dd16f452769d88c4c80.txt')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-
+// app.use('/loaderio-20c5b1e9a8571dd16f452769d88c4c80', loaderio)
 app.use('/qa', router);
 
 app.listen(PORT, () => {
