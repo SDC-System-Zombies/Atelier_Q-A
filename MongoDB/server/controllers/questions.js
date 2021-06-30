89e0a8ed41e104209ef59f8aa5e6b899b5011090
Julian Thong <julianzthong@gmail.com>
2021-06-30 15:43:34 -0700

    Created skeleton for the MongoDB server API

---
+++ MongoDB/server/controllers/questions.js
@@ -0,0 +1,16 @@
+const model = require('../models');
+
+module.exports = {
+  get: (req, res) => [
+    console.log('hello world')
+  ],
+  post: (req, res) => {
+    console.log('hello world')
+  },
+  putHelpful: (req, res) => {
+    console.log('hello world')
+  },
+  putReport: (req, res) => {
+    console.log('hello world')
+  }
+}
